7285032e3a5c4cd082febf58d9c9b3c6abb07006
Eugene Kim <eugkim1997@gmail.com>
2020-07-16 15:49:07 -0700

    video plays upon title being clicked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,42 @@ import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultVideo: exampleVideoData[0],
+      videoList: exampleVideoData
+    }
+  }
+
+  onListItemTitleClick (clickedVideo) {
+    //change state so defaultvideo property is set to clicked video
+    this.setState({
+      defaultVideo: clickedVideo
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.defaultVideo}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videoList} clickEvent={this.onListItemTitleClick.bind(this)}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -4,7 +4,7 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(currentVideo =>
-      <VideoListEntry video={currentVideo} />)}
+      <VideoListEntry video={currentVideo} clickEvent={props.clickEvent}/>)}
   </div>
 );
 
